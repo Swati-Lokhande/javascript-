98398f3e85cdeb9efb1b90dfc981cb7470ade2ed
Swati-Lokhande <163034921+Swati-Lokhande@users.noreply.github.com>
2025-11-11 13:29:51 +0530

    initial commit

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const id = 101
+let name = "swati"
+var email = "swati123@gmail.com"
+
+/* prefer to use the let keyword instead of var bacause var have issues with scope or blocks*/
+
+console.log("id = " ,id)
+console.table([id, name, email])
---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as a newer version
+// alert(5 + 10) // we are using node js, not browser
+
+let age = 22;
+let isLoggedIn = false;
+let state = null;
+
+console.table([age, isLoggedIn, state]);
+/* datatypes:
+number -> range 2 to the power of 53
+bigint
+string-> ""
+boolean
+null -> standalone value
+undefined -> if value is not aligned to variable
+symbol -> for uniqueness
+object 
+*/
+
+console.log(typeof undefined); // type of undefined is undefined only
+console.log(typeof null); // type of null is object
+
