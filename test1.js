0bc4b15a44654ea480461a37c5f8ce403bb522b3
Swati-Lokhande <163034921+Swati-Lokhande@users.noreply.github.com>
2025-11-11 13:29:51 +0530

    initial commit

---
+++ test1.js
@@ -0,0 +1,10 @@
+console.log("Welcome to js")
+const accId = 1234
+const bankId = 10101
+let name = "Swati"
+let password = "12345"
+var email = "swati @gmail.com"
+
+console.log(accId)
+
+console.table([accId, bankId, name,password,email])
