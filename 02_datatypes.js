5fcb24971a87bc90fb272b0a777f7d5996b1652d
Swati-Lokhande <163034921+Swati-Lokhande@users.noreply.github.com>
2025-11-12 11:21:43 +0530

    second commit

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -4,6 +4,9 @@
 let age = 22;
 let isLoggedIn = false;
 let state = null;
+console.log("age = ", age)
+console.log(isLoggedIn)
+console.log(state)
 
 console.table([age, isLoggedIn, state]);
 /* datatypes:
@@ -20,3 +23,72 @@ object
 console.log(typeof undefined); // type of undefined is undefined only
 console.log(typeof null); // type of null is object
 
+/************************************ */
+
+//creating a function in javascript
+
+function print(){
+  console.log("hello from function")
+}
+print()
+
+let firstName ;
+let lastName;
+function message(firstName, lastName){
+  console.log(`${firstName} ${lastName}`)
+}
+message("swati","lokhande")
+
+let address;
+let salary;
+let education;
+
+function info(address, salary,education){
+  console.log(`address= ${address} salary = ${salary} education = ${education}`);
+}
+
+info("pune", 20000, "B.Tech")
+
+
+let a, b, c;
+
+function math(a,b,c){
+  console.log(a+b+c);
+  console.log(a*b*c);
+  console.log(a/b-c);
+}
+
+math(30, 20 ,10)
+
+//arrow function
+let xyz=()=>{
+  console.log("123")
+}
+xyz()
+
+//for one line statement
+// let x;
+// let y;
+
+// let xyzq =(x,y)=>console.log(a+b)
+// xyzq(12,45)
+
+for(let i = 0; i< 20; i++)
+{
+  if(i === 13) // here when the value of i = 13 it just skip it and continue..
+  {
+    continue;
+  }
+  console.log(i); // so here we get the o/p from 0 to 19 except 13 number
+}
+
+
+//dom => Document object model
+
+/* target method in js
+    getelementbyid
+    getelementsbyclassname
+    getelementbytagname
+    queryselector
+    queryselectorall
+*/
---
+++ 03_all.js
@@ -0,0 +1,30 @@
+// type casting in java
+
+// let score = null
+// console.log("type of score is : " ,typeof(score));
+
+// let convertedInNum =Number(score);
+// console.log("type of score is : " ,typeof(convertedInNum));
+// console.log(convertedInNum)   
+
+/* o/p:
+  String -> Nan
+  number ->number
+  boolean ->number
+*/
+
+// let isEmpty = false;
+// let stringConversion = String(isEmpty)
+// console.log(typeof(stringConversion));
+// console.log("bolean is converted to String: ",stringConversion );
+
+
+// let isLoggedIn = 0
+// let login = Boolean(isLoggedIn)
+// console.log(typeof(login));
+// console.log(login);
+
+let number = 20
+let string1 = String(number)
+console.log(string1);
+console.log(typeof(string1))
